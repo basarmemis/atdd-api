3ee8a68664eaa6f523134774fbaad102e1b9e637
basarmemis <basar.memis@gmail.com>
2021-12-20 23:20:53 +0300

    ev

--- todo/todo.pact.test.js
+++ todo/todo.pact.test.js
@@ -9,13 +9,13 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(require('./todo.routes'));
-const server = app.listen("8080");
+const server = app.listen("3002");
 
 describe("Pact Verification", () => {
     it("validates the expectations of atdd-api", () => {
         const opts = {
             logLevel: "INFO",
-            providerBaseUrl: "http://localhost:8080",
+            providerBaseUrl: "http://localhost:3002",
             provider: "atdd-api",
             providerVersion: "1.0.0",
             pactUrls: [
